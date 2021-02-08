a1815824bb604cf4e54b882f192e16c76b003d32
Yovador <yovador@gmail.com>
2021-02-08 14:33:25 +0100

    RdmColor start

---
+++ jp-game-main/components/RandomColor/RandomColor.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+class Color{
+
+    constructor(name, ){
+
+    }
+
+}
+
+
+class RandomColor extends React.Component {
+
+    render() { 
+        return (
+            <View>
+                <Text>Test</Text>
+            </View>
+        );
+    }
+
+}
+ 
+export default RandomColor ;

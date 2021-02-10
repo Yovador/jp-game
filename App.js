d8815423c8547734e66cc6f21231347e5be20064
EvaVDE <eva.vderve@gmail.com>
2021-02-10 10:36:19 +0100

    accescamdeeva

--- App.js
+++ App.js
@@ -2,14 +2,24 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { Text, View } from 'react-native';
 import RandomColor from './components/RandomColor/RandomColor';
+import CameraAcces from './components/RandomColor/CameraAcces';
 import styles from './AppStyles'
+
+
+
+
+
 
 export default function App() {
   return (
     <View style={styles.container}>
       <RandomColor/>
       <StatusBar style="auto" />
+      <CameraAcces/>
     </View>
+
+
+
   );
 }
 
